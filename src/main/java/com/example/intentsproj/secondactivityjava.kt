4ef309ed51277afe2b013e8f0231db70454d89bd
sifran <sifranmohamed1996@gmail.com>
2020-03-27 20:19:56 +0530

    Tutorial 03 mad

---
+++ src/main/java/com/example/intentsproj/secondactivityjava.kt
@@ -0,0 +1,12 @@
+package com.example.intentsproj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+public class secondactivityjava : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondactivityjava)
+    }
+}
